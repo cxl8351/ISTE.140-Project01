322e5b6096f9a7079849c8ca65f69b605da1b9aa
cxl8351 <cxl8351@rit.edu>
2021-11-02 19:59:15 -0400

    Added nav.php

---
+++ inc/nav.php
@@ -0,0 +1,6 @@
+<nav id="menu">
+    <ul><a href="index.php">Home</a></ul>
+    <ul><a href="history.php">History</a></ul>
+    <ul><a href="evolution.php">Evolution</a></ul>
+    <ul><a href="debate.php">Debate</a></ul>
+</nav>
