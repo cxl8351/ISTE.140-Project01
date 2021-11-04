684150827c4f1f65e4cfd159d075718adda9276d
cxl8351 <cxl8351@rit.edu>
2021-11-04 16:19:12 -0400

    Added CSS to Nav-bar according to wireframe.

--- evolution.php
+++ evolution.php
@@ -8,6 +8,8 @@
 <html lang="en">
     <head>
         <meta charset="UTF-8">
+        <link rel="stylesheet" href="css/styles.css">
+        <link rel="stylesheet" href="css/navigation.css">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Evolution</title>
     </head>
