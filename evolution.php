bb03857c7188d11cb758560f2195a6d297a320fe
cxl8351 <cxl8351@rit.edu>
2021-11-09 19:05:33 -0500

    Modified #container</div> tags for correct closure

--- evolution.php
+++ evolution.php
@@ -20,63 +20,65 @@
         </figure>
         
         <?php include "inc/nav.php";?>
-        <div class="container">
-        <figure class = "evo_img">
-            <img src="images/nes.jpg" alt="nes">
-            <img src="images/wii.jpg" alt="wii">
-        </figure>
-        
-        <table border = "1" id="evolution">
-            <tr>
-                <th>Console</th>
-                <th>Year Released</th>
-                <th>Price</th>
-                <th>Features</th>
-                <th>Popular Games</th>
-            </tr>
-            <tr>
-                <td>Nintendo Entertainment System</td>
-                <td>1987</td>
-                <td>~$90.00</td>
-                <td>First home game console, sold 60 million units, set the standard for console and business practices, set in motion
-                    nintendos most popular games
-                </td>
-                <td>Super Mario Bros, The Legend of Zelda, Metroid, Tetris, Pacman, Megaman</td>
-            </tr>
-            <tr>
-                <td>Nintendo 64</td>
-                <td>1996</td>
-                <td>~$200.00</td>
-                <td>Increased speed, resolution and storage, sold roughly 32 million units, base model came with games, very popular with
-                    the international market
-                </td>
-                <td>Super Mario 64, Mario Kart 64, Goldeneye, Donkey Kong 64, Pokemon Snap, Star Wars</td>
-            </tr>
-            <tr>
-                <td>Wii</td>
-                <td>2006</td>
-                <td>~$250.00</td>
-                <td>Handheld motion controls, increased expansion to connect with other devices, internet connection, lower graphics,
-                    first of its kind, set to take over the gaming industry
-                </td>
-                <td>Wii sports, Mario Kart, Super Mario Bros III, Super Smash Bros, Wii Fit, Mario Party, Zelda</td>
-            </tr>
-            <tr>
-                <td>Switch</td>
-                <td>2017</td>
-                <td>~$300.00</td>
-                <td>Hybrid device from handheld to home console, wireless controls, increased graphics and resolution, incredibly popular
-                    as it only requires internet connection to work
-                </td>
-                <td>Animal Crossing, Pokemon sword and shield, Luigis Mansion 3, Super Mario Odyssey, Mario Kart 8, Zelda: Breath of the Wild</td>
-            </tr>
-        </table>
 
-        <figure class = "evo_img">
-            <img src="images/switch.jpg" alt="switch">
-            <img src="images/n64.jpg" alt="n64">
-        </figure>
-        
+        <div class="container">
+            
+            <figure class = "evo_img">
+                <img src="images/nes.jpg" alt="nes">
+                <img src="images/wii.jpg" alt="wii">
+            </figure>
+            
+            <table border = "1" id="evolution">
+                <tr>
+                    <th>Console</th>
+                    <th>Year Released</th>
+                    <th>Price</th>
+                    <th>Features</th>
+                    <th>Popular Games</th>
+                </tr>
+                <tr>
+                    <td>Nintendo Entertainment System</td>
+                    <td>1987</td>
+                    <td>~$90.00</td>
+                    <td>First home game console, sold 60 million units, set the standard for console and business practices, set in motion
+                        nintendos most popular games
+                    </td>
+                    <td>Super Mario Bros, The Legend of Zelda, Metroid, Tetris, Pacman, Megaman</td>
+                </tr>
+                <tr>
+                    <td>Nintendo 64</td>
+                    <td>1996</td>
+                    <td>~$200.00</td>
+                    <td>Increased speed, resolution and storage, sold roughly 32 million units, base model came with games, very popular with
+                        the international market
+                    </td>
+                    <td>Super Mario 64, Mario Kart 64, Goldeneye, Donkey Kong 64, Pokemon Snap, Star Wars</td>
+                </tr>
+                <tr>
+                    <td>Wii</td>
+                    <td>2006</td>
+                    <td>~$250.00</td>
+                    <td>Handheld motion controls, increased expansion to connect with other devices, internet connection, lower graphics,
+                        first of its kind, set to take over the gaming industry
+                    </td>
+                    <td>Wii sports, Mario Kart, Super Mario Bros III, Super Smash Bros, Wii Fit, Mario Party, Zelda</td>
+                </tr>
+                <tr>
+                    <td>Switch</td>
+                    <td>2017</td>
+                    <td>~$300.00</td>
+                    <td>Hybrid device from handheld to home console, wireless controls, increased graphics and resolution, incredibly popular
+                        as it only requires internet connection to work
+                    </td>
+                    <td>Animal Crossing, Pokemon sword and shield, Luigis Mansion 3, Super Mario Odyssey, Mario Kart 8, Zelda: Breath of the Wild</td>
+                </tr>
+            </table>
+
+            <figure class = "evo_img">
+                <img src="images/switch.jpg" alt="switch">
+                <img src="images/n64.jpg" alt="n64">
+            </figure>
+        </div>
     </body>
 
     <footer id = "sources">
@@ -85,5 +87,4 @@
         <li><a href="https://www.ssbwiki.com/Sora_(SSBU)">SSBU Wiki</a></li>
         <li><a href="https://www.thecrimson.com/article/2021/10/5/wthh-chris-pratt-mario/">Chris Pratt as Mario</a></li>
     </footer>
-        </div> 
 </html>
--- index.php
+++ index.php
@@ -19,11 +19,11 @@
             <img src="images/logo.png" alt="nintendo_logo">
         </figure>
         
-            <?php include "inc/nav.php";?>
+        <?php include "inc/nav.php";?>
 
         <!-- Here we try to reciprocate a carousel. As far as we believe, there is no semantically correct 'carousel' tag-->
         <div class="carousel">
-            <button class="carousel__button previous" id="previous">></button>
+            <button class="carousel__button previous" id="previous"></button>
                 <div class="carousel__images">
                     <ul>
                         <img class="active" src="images/animal_crossing.jpg" alt="roulette2">
@@ -36,100 +36,100 @@
                         <img src="images/marioworld.png" alt="roulette9">
                     </ul>
                 </div>
-            <button class="carousel__button next" id="next">></button>
+            <button class="carousel__button next" id="next"></button>
         </div>
 
         <div class="container">
-        <section id="news">
-            <h1>News and Recent Events</h1>
+            <section id="news">
+                <h1>News and Recent Events</h1>
+                <!-- An event is represented by an article. These share a 1:1 relationship. -->
+                <article id = "sora">
+                    <figure>
+                        <img src="images/sora.png" alt="fpo_image">
+                    </figure>
+                    <h2>Sora's Introduction to Super Smash Brothers: Ultimate</h2>
+                    <p>Nintendo Co., Ltd.[b] is a Japanese multinational consumer electronics and video game company headquartered in Kyoto, Japan. The company was founded in 1889 as Nintendo Karuta[c] by craftsman Fusajiro Yamauchi and originally produced handmade hanafuda playing cards. After venturing into various lines of business during the 1960s and acquiring a legal status as a public company under the current company name, Nintendo distributed its first video game console, the Color TV-Game, in 1977. It gained international recognition with the release of Donkey Kong in 1981 and the Nintendo Entertainment System and Super Mario Bros. in 1985.</p>
+                    <p>Haley Joel Osment and Miyu Irino, Sora's main voice actors in English and Japanese respectively since the first Kingdom Hearts game, reprise their roles in the English and Japanese versions of Ultimate, at least partially using re-purposed voice clips from Kingdom Hearts 3D: Dream Drop Distance.</p>
+                </article>
 
-            <!-- An event is represented by an article. These share a 1:1 relationship. -->
-            <article id = "sora">
+                <article id = "warioware">
+                    <figure>
+                        <img src="images/wario.png" alt="fpo_image">
+                    </figure>
+                    <h2>WarioWare™: Get It Together! Releases</h2>
+                    <p>For the first time, YOU control Wario and friends inside their own chaotic games. Use their distinctly absurd abilities to take on a rush of microgames. Tip a turtle with the help of Wario’s dash or choose Ashley and hurl a spell at it. In the WarioWare: Get It Together! game, mix and match characters and microgames for maximum fun!</p>
+                </article>
+
+                <article id = "metroid">
+                    <figure>
+                        <img src="images/metroid.png" alt="fpo_image">
+                    </figure>
+                    <h2>Metroid: Dread</h2>
+                    <p>Join bounty hunter Samus Aran as she tries to escape a deadly alien world plagued by a mechanical menace</p>
+                    <p>Upon investigating a mysterious transmission on Planet ZDR, Samus faces a mysterious foe that traps her in this dangerous world. The remote planet has been overrun by vicious alien lifeforms and murderous robots called E.M.M.I. Hunt or be hunted as you make your way through a labyrinth of enemies in Samus’ most intense side-scrolling adventure yet.</p>
+                </article>
+
+                <article id="nick">
+                    <figure>
+                        <img src="images/nick.png" alt="fpo_image">
+                    </figure>
+                    <h2>Nickelodeon All Star Brawl Releases on Switch</h2>
+                    <p>Brawl it out as your favorite Nickelodeon characters in bombastic platform battles! With a power-packed cast of heroes from the Nickelodeon universe, face-off with all-stars from SpongeBob Squarepants, Teenage Mutant Ninja Turtles, The Loud House, Danny Phantom, Aaahh!!! Real Monsters, The Wild Thornberrys, Hey Arnold!, Rugrats, and more to determine ultimate animation dominance. With unique move sets and attacks inspired by their personalities, each character has an individual style of play enabling endless action for Nickelodeon’s legion of fans. Select your favorite and then let the intense brawls begin with online and local multiplayer action.</p>
+                </article>
+                
+            </section>
+
+            <section id="top5">
+                <h1>Top 5 Games</h1>
                 <figure>
-                    <img src="images/sora.png" alt="fpo_image">
+                    <img src="images/fpo.png" alt="fpo_image">    
                 </figure>
-                <h2>Sora's Introduction to Super Smash Brothers: Ultimate</h2>
-                <p>Nintendo Co., Ltd.[b] is a Japanese multinational consumer electronics and video game company headquartered in Kyoto, Japan. The company was founded in 1889 as Nintendo Karuta[c] by craftsman Fusajiro Yamauchi and originally produced handmade hanafuda playing cards. After venturing into various lines of business during the 1960s and acquiring a legal status as a public company under the current company name, Nintendo distributed its first video game console, the Color TV-Game, in 1977. It gained international recognition with the release of Donkey Kong in 1981 and the Nintendo Entertainment System and Super Mario Bros. in 1985.</p>
-                <p>Haley Joel Osment and Miyu Irino, Sora's main voice actors in English and Japanese respectively since the first Kingdom Hearts game, reprise their roles in the English and Japanese versions of Ultimate, at least partially using re-purposed voice clips from Kingdom Hearts 3D: Dream Drop Distance.</p>
-            </article>
 
-            <article id = "warioware">
                 <figure>
-                    <img src="images/wario.png" alt="fpo_image">
+                    <img src="images/fpo.png" alt="fpo_image">    
                 </figure>
-                <h2>WarioWare™: Get It Together! Releases</h2>
-                <p>For the first time, YOU control Wario and friends inside their own chaotic games. Use their distinctly absurd abilities to take on a rush of microgames. Tip a turtle with the help of Wario’s dash or choose Ashley and hurl a spell at it. In the WarioWare: Get It Together! game, mix and match characters and microgames for maximum fun!</p>
-            </article>
 
-            <article id = "metroid">
                 <figure>
-                    <img src="images/metroid.png" alt="fpo_image">
+                    <img src="images/fpo.png" alt="fpo_image">
                 </figure>
-                <h2>Metroid: Dread</h2>
-                <p>Join bounty hunter Samus Aran as she tries to escape a deadly alien world plagued by a mechanical menace</p>
-                <p>Upon investigating a mysterious transmission on Planet ZDR, Samus faces a mysterious foe that traps her in this dangerous world. The remote planet has been overrun by vicious alien lifeforms and murderous robots called E.M.M.I. Hunt or be hunted as you make your way through a labyrinth of enemies in Samus’ most intense side-scrolling adventure yet.</p>
-            </article>
 
-            <article id="nick">
+                <figure class = "bot2">
+                    <img src="images/fpo.png" alt="fpo_image">    
+                </figure>
+
+                <figure class = "bot2">
+                    <img src="images/fpo.png" alt="fpo_image">    
+                </figure>
+            </section>
+
+            <section id="brief">
                 <figure>
-                    <img src="images/nick.png" alt="fpo_image">
+                    <img src="images/brief.jpg" alt="brief_img">
                 </figure>
-                <h2>Nickelodeon All Star Brawl Releases on Switch</h2>
-                <p>Brawl it out as your favorite Nickelodeon characters in bombastic platform battles! With a power-packed cast of heroes from the Nickelodeon universe, face-off with all-stars from SpongeBob Squarepants, Teenage Mutant Ninja Turtles, The Loud House, Danny Phantom, Aaahh!!! Real Monsters, The Wild Thornberrys, Hey Arnold!, Rugrats, and more to determine ultimate animation dominance. With unique move sets and attacks inspired by their personalities, each character has an individual style of play enabling endless action for Nickelodeon’s legion of fans. Select your favorite and then let the intense brawls begin with online and local multiplayer action.</p>
-            </article>
-            
-        </section>
-
-        <section id="top5">
-            <h1>Top 5 Games</h1>
-            <figure>
-                <img src="images/fpo.png" alt="fpo_image">    
-            </figure>
-
-            <figure>
-                <img src="images/fpo.png" alt="fpo_image">    
-            </figure>
-
-            <figure>
-                <img src="images/fpo.png" alt="fpo_image">
-            </figure>
-
-            <figure class = "bot2">
-                <img src="images/fpo.png" alt="fpo_image">    
-            </figure>
-
-            <figure class = "bot2">
-                <img src="images/fpo.png" alt="fpo_image">    
-            </figure>
-        </section>
-
-        <section id="brief">
-            <figure>
-                <img src="images/brief.jpg" alt="brief_img">
-            </figure>
-            
-            <p>
-                Nintendo Co., Ltd.[b] is a Japanese multinational consumer electronics and video game company headquartered in Kyoto, Japan.
-                The company was founded in 1889 as Nintendo Karuta[c] by craftsman Fusajiro Yamauchi and originally produced handmade 
-                hanafuda playing cards. After venturing into various lines of business during the 1960s and acquiring a legal status as a 
-                public company under the current company name, Nintendo distributed its first video game console, the Color TV-Game, in 
-                1977. It gained international recognition with the release of Donkey Kong in 1981 and the Nintendo Entertainment System 
-                and Super Mario Bros. in 1985.
-            </p>
-            <p>
-                Since then, Nintendo has produced some of the most successful consoles in the video game industry, such as the Game Boy, 
-                the Super Nintendo Entertainment System, the Nintendo DS, the Wii, and the Nintendo Switch. It has created numerous major 
-                franchises, including Mario, Donkey Kong, The Legend of Zelda, Pokémon, Kirby, Metroid, Fire Emblem, Animal Crossing, 
-                Splatoon, Star Fox, Xenoblade Chronicles, and Super Smash Bros. The character of Mario is internationally recognisable, 
-                and serves as the company's mascot.
-            </p>
-            <p>
-                Nintendo has multiple subsidiaries in Japan and abroad, in addition to business partners such as The Pokémon Company and 
-                HAL Laboratory. Nintendo and its staff has received awards including Emmy Awards for Technology & Engineering, Game Awards, 
-                Game Developers Choice Awards and British Academy Games Awards. It is one of the wealthiest and most valuable companies 
-                in the Japanese market.
-            </p>
-        </section>
+                
+                <p>
+                    Nintendo Co., Ltd.[b] is a Japanese multinational consumer electronics and video game company headquartered in Kyoto, Japan.
+                    The company was founded in 1889 as Nintendo Karuta[c] by craftsman Fusajiro Yamauchi and originally produced handmade 
+                    hanafuda playing cards. After venturing into various lines of business during the 1960s and acquiring a legal status as a 
+                    public company under the current company name, Nintendo distributed its first video game console, the Color TV-Game, in 
+                    1977. It gained international recognition with the release of Donkey Kong in 1981 and the Nintendo Entertainment System 
+                    and Super Mario Bros. in 1985.
+                </p>
+                <p>
+                    Since then, Nintendo has produced some of the most successful consoles in the video game industry, such as the Game Boy, 
+                    the Super Nintendo Entertainment System, the Nintendo DS, the Wii, and the Nintendo Switch. It has created numerous major 
+                    franchises, including Mario, Donkey Kong, The Legend of Zelda, Pokémon, Kirby, Metroid, Fire Emblem, Animal Crossing, 
+                    Splatoon, Star Fox, Xenoblade Chronicles, and Super Smash Bros. The character of Mario is internationally recognisable, 
+                    and serves as the company's mascot.
+                </p>
+                <p>
+                    Nintendo has multiple subsidiaries in Japan and abroad, in addition to business partners such as The Pokémon Company and 
+                    HAL Laboratory. Nintendo and its staff has received awards including Emmy Awards for Technology & Engineering, Game Awards, 
+                    Game Developers Choice Awards and British Academy Games Awards. It is one of the wealthiest and most valuable companies 
+                    in the Japanese market.
+                </p>
+            </section>
+        </div>
     </body>
 
     <footer id="sources">
@@ -173,5 +173,4 @@
         });
         });
     </script>
-        </div>
 </html>
